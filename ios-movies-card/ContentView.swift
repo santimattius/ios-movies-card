def45ace43641dec45346e9345d75de730243fba
Santiago Mattiauda <santimattius@gmail.com>
2021-12-11 18:42:59 -0300

    Initial Commit

---
+++ ios-movies-card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-movies-card
+//
+//  Created by Santiago Mattiauda on 11/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-movies-card/ios_movies_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_movies_cardApp.swift
+//  ios-movies-card
+//
+//  Created by Santiago Mattiauda on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_movies_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
